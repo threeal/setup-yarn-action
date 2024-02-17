57dc610b3b9242d0171709637fb9e531021bb0a3
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-17 17:32:30 +0700

    test: add test for `yarn.install` function

--- src/yarn.test.mjs
+++ src/yarn.test.mjs
@@ -33,3 +33,20 @@ it("should enable Yarn", async () => {
   expect(exec).toHaveBeenCalledTimes(1);
   expect(exec).toHaveBeenCalledWith("corepack", ["enable", "yarn"]);
 });
+
+it("should install package using Yarn", async () => {
+  const { exec } = await import("@actions/exec");
+  const yarn = (await import("./yarn.mjs")).default;
+
+  await yarn.install();
+
+  expect(exec).toHaveBeenCalledTimes(1);
+  expect(exec).toHaveBeenCalledWith("corepack", ["yarn", "install"], {
+    env: {
+      ...process.env,
+      GITHUB_ACTIONS: "",
+      FORCE_COLOR: "true",
+      CI: "",
+    },
+  });
+});
