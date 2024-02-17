195142a3560105346a52c9fff7eca4e5b61750bc
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-17 17:32:29 +0700

    test: add test for `yarn.enable` function

--- src/yarn.test.mjs
+++ src/yarn.test.mjs
@@ -4,6 +4,10 @@ jest.unstable_mockModule("@actions/exec", () => ({
   exec: jest.fn(),
 }));
 
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
 it("should disable Yarn global cache", async () => {
   const { exec } = await import("@actions/exec");
   const yarn = (await import("./yarn.mjs")).default;
@@ -18,4 +22,14 @@ it("should disable Yarn global cache", async () => {
     "enableGlobalCache",
     "false",
   ]);
+});
+
+it("should enable Yarn", async () => {
+  const { exec } = await import("@actions/exec");
+  const yarn = (await import("./yarn.mjs")).default;
+
+  await yarn.enable();
+
+  expect(exec).toHaveBeenCalledTimes(1);
+  expect(exec).toHaveBeenCalledWith("corepack", ["enable", "yarn"]);
 });
