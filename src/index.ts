7a534b3de48b9555562f5ac7ab72eafabf068670
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-19 19:53:55 +0700

    feat: allow getting cache key if lock file does not exist

--- src/index.ts
+++ src/index.ts
@@ -9,37 +9,29 @@ async function main(): Promise<void> {
   });
 
   const cacheKey = await core.group("Getting cache key", getCacheKey);
+  const cachePaths = await core.group("Getting cache paths", getCachePaths);
 
-  let cachePaths: string[] = [];
-  if (cacheKey !== undefined) {
-    cachePaths = await core.group("Getting cache paths", getCachePaths);
-  }
-
-  if (cacheKey !== undefined) {
-    const cacheFound = await core.group("Restoring cache", async () => {
-      const cacheId = await cache.restoreCache(cachePaths.slice(), cacheKey);
-      if (cacheId === undefined) {
-        core.warning("Cache not found");
-        return false;
-      }
-      return true;
-    });
-
-    if (cacheFound) {
-      core.info("Cache restored successfully");
-      return;
+  const cacheFound = await core.group("Restoring cache", async () => {
+    const cacheId = await cache.restoreCache(cachePaths.slice(), cacheKey);
+    if (cacheId === undefined) {
+      core.warning("Cache not found");
+      return false;
     }
+    return true;
+  });
+
+  if (cacheFound) {
+    core.info("Cache restored successfully");
+    return;
   }
 
   await core.group("Installing dependencies", async () => {
     return yarnInstall();
   });
 
-  if (cacheKey !== undefined) {
-    await core.group("Saving cache", async () => {
-      return cache.saveCache(cachePaths.slice(), cacheKey);
-    });
-  }
+  await core.group("Saving cache", async () => {
+    return cache.saveCache(cachePaths.slice(), cacheKey);
+  });
 }
 
 main().catch((err) => core.setFailed(err));
