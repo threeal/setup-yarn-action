94a911c7e2011469ab5f84736d9d8a51d71ff785
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-22 13:54:33 +0700

    feat: skip cache if `cache` input is `false`

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,12 @@ import * as cache from "@actions/cache";
 import * as core from "@actions/core";
 import { getCacheKey, getCachePaths } from "./cache.js";
 import { enableYarn, yarnInstall } from "./yarn/index.js";
+import { getInputs } from "./inputs.js";
 
 export async function main(): Promise<void> {
+  core.info("Getting action inputs...");
+  const inputs = getInputs();
+
   core.info("Enabling Yarn...");
   try {
     await enableYarn();
@@ -12,46 +16,48 @@ export async function main(): Promise<void> {
     return;
   }
 
-  core.startGroup("Getting cache key");
   let cacheKey: string;
-  try {
-    cacheKey = await getCacheKey();
-  } catch (err) {
-    core.endGroup();
-    core.setFailed(`Failed to get cache key: ${err.message}`);
-    return;
-  }
-  core.endGroup();
-
-  core.startGroup("Getting cache paths");
   let cachePaths: string[];
-  try {
-    cachePaths = await getCachePaths();
-  } catch (err) {
-    core.endGroup();
-    core.setFailed(`Failed to get cache paths: ${err.message}`);
-    return;
-  }
-  core.endGroup();
-
-  core.startGroup("Restoring cache");
-  let cacheFound: boolean;
-  try {
-    const cacheId = await cache.restoreCache(cachePaths.slice(), cacheKey);
-    cacheFound = cacheId != undefined;
-    if (!cacheFound) {
-      core.warning("Cache not found");
+  if (inputs.cache) {
+    core.startGroup("Getting cache key");
+    try {
+      cacheKey = await getCacheKey();
+    } catch (err) {
+      core.endGroup();
+      core.setFailed(`Failed to get cache key: ${err.message}`);
+      return;
     }
-  } catch (err) {
     core.endGroup();
-    core.setFailed(`Failed to restore cache: ${err.message}`);
-    return;
-  }
-  core.endGroup();
 
-  if (cacheFound) {
-    core.info("Cache restored successfully");
-    return;
+    core.startGroup("Getting cache paths");
+    try {
+      cachePaths = await getCachePaths();
+    } catch (err) {
+      core.endGroup();
+      core.setFailed(`Failed to get cache paths: ${err.message}`);
+      return;
+    }
+    core.endGroup();
+
+    core.startGroup("Restoring cache");
+    let cacheFound: boolean;
+    try {
+      const cacheId = await cache.restoreCache(cachePaths.slice(), cacheKey);
+      cacheFound = cacheId != undefined;
+      if (!cacheFound) {
+        core.warning("Cache not found");
+      }
+    } catch (err) {
+      core.endGroup();
+      core.setFailed(`Failed to restore cache: ${err.message}`);
+      return;
+    }
+    core.endGroup();
+
+    if (cacheFound) {
+      core.info("Cache restored successfully");
+      return;
+    }
   }
 
   core.startGroup("Installing dependencies");
@@ -64,13 +70,15 @@ export async function main(): Promise<void> {
   }
   core.endGroup();
 
-  core.startGroup("Saving cache");
-  try {
-    await cache.saveCache(cachePaths.slice(), cacheKey);
-  } catch (err) {
+  if (inputs.cache) {
+    core.startGroup("Saving cache");
+    try {
+      await cache.saveCache(cachePaths.slice(), cacheKey);
+    } catch (err) {
+      core.endGroup();
+      core.setFailed(`Failed to save cache: ${err.message}`);
+      return;
+    }
     core.endGroup();
-    core.setFailed(`Failed to save cache: ${err.message}`);
-    return;
   }
-  core.endGroup();
 }
