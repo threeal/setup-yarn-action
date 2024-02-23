313d0094a850d77345b49805fd41f0c05a63f8e9
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-23 22:25:46 +0700

    feat: add support to listen to std line in `execYarn` function

--- src/yarn/exec.test.ts
+++ src/yarn/exec.test.ts
@@ -8,10 +8,14 @@ it("should execute a Yarn command", async () => {
   const { exec } = await import("@actions/exec");
   const { execYarn } = await import("./exec.js");
 
-  await execYarn(["do", "something"]);
+  const callback = () => {};
+  await execYarn(["do", "something"], callback);
 
   expect(exec).toHaveBeenCalledTimes(1);
   expect(exec).toHaveBeenCalledWith("corepack", ["yarn", "do", "something"], {
     silent: true,
+    listeners: {
+      stdline: callback,
+    },
   });
 });
--- src/yarn/exec.ts
+++ src/yarn/exec.ts
@@ -3,8 +3,20 @@ import { exec } from "@actions/exec";
 /**
  * Execute a Yarn command with the given arguments.
  *
+ * This function executes the Yarn command silently, without outputting anything to the standard output.
+ * Optionally, a callback can be provided to read the output from the command.
+ *
  * @param args - The arguments of the Yarn command.
+ * @param stdlineCallback - A callback to be called when receiving a line from the standard output.
  */
-export async function execYarn(args?: string[]): Promise<void> {
-  await exec("corepack", ["yarn", ...args], { silent: true });
+export async function execYarn(
+  args?: string[],
+  stdlineCallback?: (data: string) => void,
+): Promise<void> {
+  await exec("corepack", ["yarn", ...args], {
+    silent: true,
+    listeners: {
+      stdline: stdlineCallback,
+    },
+  });
 }
