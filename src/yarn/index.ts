8e054a37e45ea7f276e0c6646bf3e0c82d88422b
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-25 18:09:26 +0700

    feat: add step in `enableYarn` function to check if using the same Yarn version

--- src/yarn/index.ts
+++ src/yarn/index.ts
@@ -1,9 +1,19 @@
 import { exec, getExecOutput } from "@actions/exec";
+import { getYarnVersion } from "./version.js";
 export { yarnInstall } from "./install.js";
 export { getYarnVersion } from "./version.js";
 
 export async function enableYarn(): Promise<void> {
   await exec("corepack", ["enable", "yarn"], { silent: true });
+
+  // Check if the `yarn` command is using the same version as the `corepack yarn` command.
+  const version = await getYarnVersion();
+  const corepackVersion = await getYarnVersion({ corepack: true });
+  if (version != corepackVersion) {
+    throw new Error(
+      `The \`yarn\` command is using different version of Yarn, expected \`${corepackVersion}\` but got \`${version}\``,
+    );
+  }
 }
 
 export async function getYarnConfig(name: string): Promise<string> {
